e1e31dc841f6fe4792c83bbc7b3883b7720a85f0
David P. Steelman <dsteelma@umd.edu>
2021-03-12 14:08:56 -0500

    LIBITD-1869. Added simple sanity test for verify Jenkins test activation
    Added a simple "sanity test" to verify that Jenkins properly handles
    tests.
    
    https://issues.umd.edu/browse/LIBITD-1869

---
+++ test/models/sanity_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+# Simple sanity test to verify that tests are working
+class SanityTest < ActiveSupport::TestCase
+  test 'tests can be run' do
+    assert true
+  end
+end
