665a1e5c0735ad65bb211d5b808a0c3e317b4022
David P. Steelman <dsteelma@umd.edu>
2021-03-23 09:02:35 -0400

    LIBITD-1869. Modified system tests to use "headless" Chrome
    Replaced "selenium" driver in "test/application_system_test_case.rb"
    with a "chrome_headless" configuration, so that individual Chrome
    instances would not display when running the tests.
    
    https://issues.umd.edu/browse/LIBITD-1869

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,18 @@
 
 require 'test_helper'
 
+# Base class for integration/system tests
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  Capybara.register_driver :chrome_headless do |app|
+    options = ::Selenium::WebDriver::Chrome::Options.new
+
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1400,1400')
+
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
+
+  driven_by :chrome_headless
 end
